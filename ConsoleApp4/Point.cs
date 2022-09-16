a230dad38ab40ed3c227663b7ff9f337e4a40e5f
KumeoSang <doansang654@gmail.com>
2022-09-16 23:21:19 +0700

    Add project files.

---
+++ ConsoleApp4/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Point()
+        {
+
+
+        }
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+        public string ToString()
+        {
+            return $"{X},{Y}";
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using BoxPacker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var packer = new RotatePacker();
+
+            Random rand = new Random();
+
+            for (uint i = 1; i < 10; i++)
+            {
+                RotatePacker.Box box = new RotatePacker.Box()
+                {
+                    width = i * 100,
+                    height = 500
+                };
+                packer.AddBox(box);
+            }
+            packer.Pack(1300, 1000);
+            var a = packer.boxes.Where(c => c.position != null).ToList();
+            
+        }
+    }
+}
